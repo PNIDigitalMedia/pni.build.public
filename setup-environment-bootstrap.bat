d14955d619e90f054644b8d5c9c6f713c7ff2f96
ikarpenkov <ikarpenkov@pnimedia.com>
2017-03-07 13:59:02 -0800

    created bootstrap script that will run setup-environment.bat script from phoenix repository

---
+++ setup-environment-bootstrap.bat
@@ -0,0 +1,40 @@
+:: don't display commands from this script in terminal
+@echo off
+
+:: make sure that we are running script as admin
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Error: You need to run this script as ADMIN.
+  pause
+  exit /b	
+)
+
+:: move to pni-platform folder
+for %%* in (.) do set CurrDirName=%%~nx*
+if %CurrDirName% neq pni-platform (
+  ECHO Error: something is wrong with the script. Current folder should be pni-platform but was %CurrDirName%
+  pause
+  exit /b	
+)
+SET "platformPath=%cd%"
+
+:: make sure that there is a pni-platform\phoenix folder, otherwise it means that script has been executed already and we should stop execution
+if exist phoenix (
+  echo pni-platform\phoenix folder already exists. Script aborted.
+  pause
+  exit /b
+)
+
+:: install chocolatey
+powershell -noProfile -executionPolicy bypass -command "& {iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex }"
+SET PATH=%PATH%;C:\ProgramData\chocolatey\bin
+
+:: install git
+choco install git -y
+SET PATH=%PATH%;C:\Program Files\Git\cmd
+
+:: clone phoenix repository and switch to develop branch
+git clone  http://vantfsapp.photochannel.net:8080/tfs/SharedServices/_git/PHX/ phoenix --branch feature/cake-script
+
+:: call rest of the script from a private repository
+call phoenix\Build\setup-environment.bat
---
+++ setup-environment-readme.txt
@@ -0,0 +1,3 @@
+copy and paste the following line into a command prompt with admin permissions: 
+
+powershell -noProfile -executionPolicy bypass -command "mkdir pni-platform; cd pni-platform; wget https://raw.githubusercontent.com/PNIDigitalMedia/pni.build/master/setup-environment-bootstrap.bat -outfile setup-environment-bootstrap.bat ; .\setup-environment-bootstrap.bat"
